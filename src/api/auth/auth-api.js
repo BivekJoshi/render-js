cc19acbc46c1a7c2df0edc718b31fedc3c882ac3
bivek.joshi <bivek.joshi@dghub.io>
2024-01-27 23:13:47 +0545

    changes

--- src/api/auth/auth-api.js
+++ src/api/auth/auth-api.js
@@ -2,11 +2,20 @@ import { axiosInstance } from "../axiosInterceptor";
 
 /*________________________LOGIN_____________________________________*/
 export const login = async (email, password) => {
-  const { data } = await axiosInstance.post("/v1/authenticate", {
-    email,
-    password,
-  });
-  return data;
+  try {
+    const { data } = await axiosInstance.post("/v1/authenticate", {
+      email,
+      password,
+    });
+    return data;
+  } catch (error) {
+    if (error.response && error.response.status === 500) {
+      alert("Server is down or unreachable. Please stay connected !!!.");
+    } else {
+      alert("Server is down or unreachable. Please stay connected !!!.");
+    }
+    throw error;
+  }
 };
 
 /*________________________GET LOGGEDIN USER DETAIL_____________________________________*/
@@ -17,13 +26,13 @@ export const getLoggedinUserDetail = async () => {
 
 /*________________________FORGET PASSWORD_____________________________________*/
 export const forgotPassword = async (formData) => {
-  const data = await axiosInstance.post(`/v1/user/forgot-password`,formData);
+  const data = await axiosInstance.post(`/v1/user/forgot-password`, formData);
   return data;
 };
 
 /*________________________CHANGE PASSWORD_____________________________________*/
 export const changePassword = async (formData) => {
   const { confirmPassword, ...dataToSend } = formData;
-  const data = await axiosInstance.post(`/v1/user/change-password`,dataToSend);
+  const data = await axiosInstance.post(`/v1/user/change-password`, dataToSend);
   return data;
 };
